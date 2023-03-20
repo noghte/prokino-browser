88d87f1094cc18e0b857c73b0c7b0474dff2bd6b
Saber Soleymani <postorder@gmail.com>
2023-03-20 11:18:54 -0400

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,127 +20,26 @@ export default function About() {
         <div id="top" className="container mbr-black">
             <div>
             <div class="title col-12 col-md-12">
-            <h4 class="display-5">About Browser</h4>
-            <AboutBrowser />
 
-            <h4 class="display-5" style={{marginTop:'2rem'}}>Schema</h4>
-            <AboutSchema />
-            
-            <AboutPeople style={{marginTop:'2rem'}} />
+            <h4 class="display-5">About Ontology</h4>
+            <AboutOntology />
 
             <h4 class="display-5" style={{marginTop:'2rem'}}>Data Sources</h4>
             <AboutDataSources />
 
+            <h4 class="display-5" style={{marginTop:'2rem'}}>Schema</h4>
+            <AboutSchema />
+
+            <h4 class="display-5">About Browser</h4>
+            <AboutBrowser />
+
+            <AboutPeople style={{marginTop:'2rem'}} />
+
+    
+
           </div>
                 </div>
-                {/* <Nav tabs >
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: activeTab === 'ontology' })}
-                            onClick={() => { toggle('ontology'); }}>
-                            Ontology
-                        </NavLink>
-                    </NavItem>
-
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: activeTab === 'schema' })}
-                            onClick={() => { toggle('schema'); }}>
-                            Schema
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: activeTab === 'datasources' })}
-                            onClick={() => { toggle('datasources'); }}>
-                            Data Sources
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: activeTab === 'browser' })}
-                            onClick={() => { toggle('browser'); }}>
-                            Browser
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: activeTab === 'people' })}
-                            onClick={() => { toggle('people'); }}>
-                            People
-                        </NavLink>
-                    </NavItem>
-                </Nav> */}
-                {/* <TabContent activeTab={activeTab}>
-                    <TabPane tabId="ontology">
-                        <Row>
-                            <Col sm="12">
-                                <div className="row">
-                                <div className="favth-col-xs-6">
-
-                                    <br />
-              
-                                </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId="schema">
-                        <Row>
-                            <Col sm="12">
-                                <div className="row">
-                                    <div className="favth-col-xs-6">
-                                        <br />
-                        
-                                        <AboutSchema />
-                            
-                                    </div>
-
-                                </div>
-                            </Col>
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId="datasources">
-                        <Row>
-                            <Col sm="12">
-                                <div className="row">
-                                    <div className="favth-col-xs-6">
-                                    <br />
-                
-                                        <AboutDataSources />
-
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId="browser">
-                        <Row>
-                            <Col sm="12">
-                                <br />
-                                <div className="row">
-                                    <div className="favth-col-xs-6">
-                                        <AboutBrowser />
-                              
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId="people">
-                        <Row>
-                            <Col sm="12">
-                                <br />
-                                <div className="row">
-                                    <div className="favth-col-xs-6">
-                                        <AboutPeople />
-                              
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    </TabPane>
-                </TabContent> */}
+            
         </div>
 
     </Layout>)
